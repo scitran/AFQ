ff36a2c2f9680565464f8b392700b3117136948a
Garikoitz <garikoitz@gmail.com>
2019-02-01 09:23:48 -0800

    visualize tck files in mrtrix, and create obj files too

---
+++ vis/AFQ_FgToTck.m
@@ -0,0 +1,14 @@
+function AFQ_FgToTck(fg, pathName, fib2mt, createObj)
+    for fiberNum  = 1:length(fg)
+        myfg      = fg(fiberNum); 
+        [fiberPath, fname, fext] = fileparts(pathName);
+        % Add the name of .mat this tracts originated
+        fiberName = [fname '_' strrep(myfg.name,' ','') '.tck'];
+        objName   = [fname '_' strrep(myfg.name,' ','') '.obj'];
+        % Usually tck files stored in mrtrix folder
+        if fib2mt;fiberPath = strrep(fiberPath,'fibers','mrtrix'); end
+        if ~exist(fiberPath,'dir'); mkdir(fiberPath);end
+        dr_fwWriteMrtrixTck(myfg, fullfile(fiberPath, fiberName))
+        if createObj;fg2MNIObj(myfg,'fname', fullfile(fiberPath,objName));end
+    end
+end
